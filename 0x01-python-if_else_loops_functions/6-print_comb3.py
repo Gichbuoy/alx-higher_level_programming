d585916625ae6f974067572291909bc315ef314b
Alex Maina <lexizgichbuoy@gmail.com>
2023-05-09 12:56:39 -0700

    print_combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""function that prints all possible combination of 2 digits """
 
 
-for a in range(0, 10):
-    for b in range(a + 1, 10):
-        if a == 8 and y == 9:
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
             print("89")
         else:
-            print("{}{}, ".format(a, b), end="")
+            print("{}{}, ".format(x, y), end="")
