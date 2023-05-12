95c47c8adb3171dcffc026c0585b7d0adcc01e6d
Alex Maina <lexizgichbuoy@gmail.com>
2023-05-12 10:33:22 -0700

    replace_element_copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that replaces an element without modiying the original"""
+
+
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+    if idx < 0 | idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        return copy
