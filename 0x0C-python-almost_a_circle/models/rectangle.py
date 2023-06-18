4f9fef2542a0b02ea33b3f3528fbf0eeb1913d66
Alex Maina <lexizgichbuoy@gmail.com>
2023-06-18 06:58:17 -0700

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""function that defines class Rectangle, that inherits from Base:"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class that represents a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize variables for the new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """sets the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
