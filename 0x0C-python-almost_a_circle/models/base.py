169df9a1b74cf7bac06314be6e91c910fc64568d
Alex Maina <lexizgichbuoy@gmail.com>
2023-06-18 09:59:12 -0700

    dict-instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,60 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method that writes the JSON string representation of list_objs
+        Args:
+            list_objs (list): A list of inherited Base istance.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """class method that decentralizes a JSON string.
+        Args:
+            json_string (str): A json string representation of a list of dicts.
+        Returns:
+            If json_string is None or empty
+            Otherwise the python list repreented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """class method that returns class instantiated from a dict of attr.
+        Args:
+            *dictionary: key value pair of attributes to initialize
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """class method that returns a list of classes instantiated from a file
+        Args:
+            reads from '<cls.__name__>.json'
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
