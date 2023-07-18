01b1da2044a4ee65d5c10f7bc23842c427f9992f
Alex Maina <lexizgichbuoy@gmail.com>
2023-07-18 15:26:14 -0700

    first_add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---script that inserts a new row in the table first_table in MySQL serve.
+-- script that inserts a new row in the table first_table in MySQL serve.
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
