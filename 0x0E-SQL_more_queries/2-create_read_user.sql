7f254b2d33f5611a83189a86caea1d249ac1a538
Alex Maina <lexizgichbuoy@gmail.com>
2023-07-19 13:17:52 -0700

    read_user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,7 +6,7 @@
 CREATE DATABASE
 	IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-	IF NOT EXISTS 'USER_0d_2'@'localhost'
+	IF NOT EXISTS 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_pwd';
 GRANT SELECT
 	ON `hbtn_0d_2`.*
