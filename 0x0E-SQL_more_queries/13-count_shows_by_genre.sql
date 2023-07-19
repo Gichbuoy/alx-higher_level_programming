1b70913de0c6d254f86e2a100e9858152ca49938
Alex Maina <lexizgichbuoy@gmail.com>
2023-07-19 12:30:28 -0700

    genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- Second column must be called number_of_shows
+
