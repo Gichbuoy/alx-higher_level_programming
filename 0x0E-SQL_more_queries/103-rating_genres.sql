e779d9ce0954040315e4b632ed140ccc39dd4ce9
Alex Maina <lexizgichbuoy@gmail.com>
2023-07-20 13:48:31 -0700

    best_genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_genres.name - rating sum
 -- Results must be sorted in descending order by their rating
--- You can use only one SELECT statement
+-- use only one SELECT statement
 SELECT g.name, SUM(r.rate) AS rating
 	FROM tv_genres g
 	INNER JOIN tv_show_genres s ON s.genre_id = g.id
