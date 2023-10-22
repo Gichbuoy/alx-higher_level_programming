65c356a52acdcc62b5eebdb9f14e18d6970d8a14
Alex Maina <116671634+Gichbuoy@users.noreply.github.com>
2023-10-22 22:27:07 +0300

    web_query

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -3,6 +3,6 @@ $('document').ready(function () {
     $('INPUT#btn_translate').click(function () {
       $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
         $('DIV#hello').html(data.hello);
-      });
     });
   });
+});
